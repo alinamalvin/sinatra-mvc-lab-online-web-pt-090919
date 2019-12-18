9203955e6e00ca5ff525d3a505f734c5fd573b0b
Alina Kornilova <alinakornilova78@gmail.com>
2019-12-18 15:10:58 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,15 @@ class TextAnalyzer
     b = a.map {|word| piglatinize_word(word)}
     b.join(" ")
 end
-
+ 
+  def piglatinize_word(word)
+    first_letter = word[0].downcase
+    if first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u"
+        # piglatinize word that starts with a vowel
+    else
+        # piglatinize word that starts with a consonant
+    end
+end
 
 
  
