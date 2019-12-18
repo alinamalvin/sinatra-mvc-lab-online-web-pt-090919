c921c7a9e30b5f03bde904627dc501451daa58af
Alina Kornilova <alinakornilova78@gmail.com>
2019-12-18 15:06:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
   
   post '/' do
-  @analyzed_text = TextAnalyzer.new(params[:user_text])
+  @analyzed_text = TextAnalyzer.new(params[:user_input])
  
   erb :results
 end
