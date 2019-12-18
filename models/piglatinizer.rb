bc429a3bba2f5a1105547b5887996adc7729e688
Alina Kornilova <alinakornilova78@gmail.com>
2019-12-18 15:12:56 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,44 +16,18 @@ end
     if ["a", "e", "i", "o", "u"].include?(first_letter)
         "#{word}way"
     else
-        # piglatinize word that starts with a consonant
+      consonants = []
+      consonants << word[0]
+        if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+          consonants << word[1]
+          if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+            consonants << word[2]
+          end
+        end
+      "#{word[consonants.length..-1] + consonants.join + "ay"}"
     end
 end
 
 
  
-  def count_of_words
-    words = text.split(" ")
-    words.count
-  end
- 
-  def count_of_vowels
-    text.scan(/[aeoui]/).count
-  end
- 
-  def count_of_consonants
-    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
-  end
- 
-  def most_used_letter
-    s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
-    arr = s1.split('')
-    arr1 = arr.uniq
-    arr2 = {}
- 
-    arr1.map do |c|
-      arr2[c] =  arr.count(c)
-    end
- 
-    biggest = { arr2.keys.first => arr2.values.first }
- 
-    arr2.each do |key, value|
-      if value > biggest.values.first
-        biggest = {}
-        biggest[key] = value
-      end
-    end
- 
-    biggest
-  end
-end
+  
